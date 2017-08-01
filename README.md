2858d3c05613c58a52cc93136904a5bb3cc65427
joonghyun <jjoonghyun@zuminternet>
2017-08-01 18:41:26 +0900

    freemarker 추가, devtools 추가, 기본 프로젝트 구성

---
+++ README.md
@@ -0,0 +1,1 @@
+# Backbone.js 공부
---
+++ src/main/java/com/study/backbone/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.study.backbone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping("/")
+    public String main(Model model) {
+        return "/main/main";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        return "/home/home";
+    }
+
+    @GetMapping("/articles")
+    public String articles(Model model) {
+        return "/article/list";
+    }
+
+    @GetMapping("/articles/form")
+    public String articlesForm(Model model) {
+        return "/article/form";
+    }
+
+}
