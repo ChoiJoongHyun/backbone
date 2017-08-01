8c8c35c4ce6f8957d8ed78c9445aa33a1934a0fe
joonghyun <jjoonghyun@zuminternet>
2017-08-01 17:52:07 +0900

    first commit

---
+++ src/main/java/com/study/backbone/BackboneApplication.java
@@ -0,0 +1,12 @@
+package com.study.backbone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackboneApplication.class, args);
+	}
+}
